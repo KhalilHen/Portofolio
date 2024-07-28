66db55866b20a9d4f1cce0ceb14c8ab88f0d271b
Khalil <khalilhenane@hotmail.com>
2024-07-28 15:44:06 +0200

    Worked on navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './css/navbar.css';
+
+
+const  NavBar = () => {
+  return (
+
+    
+    <div className='navbar-container'>
+
+
+        <div className='
+    navbar-items'>
+<ul>
+{/* <i className='fas fa-band-aid'></i><li>Icon</li>    */}
+<li>Khalil</li>
+<li>About me</li>
+<li>Projects</li>
+<li>Contact</li>
+
+</ul>
+</div>
+ </div>
+
+
+
+ 
+);
+ 
+}
+
+export default NavBar;
