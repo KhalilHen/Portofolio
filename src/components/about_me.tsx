818fc126400a43d9b1236cb8b99bc3f49bc0e874
Khalil <khalilhenane@hotmail.com>
2024-08-07 21:15:49 +0200

    Made the About me header text, and worked further on the rest

--- src/components/about_me.tsx
+++ src/components/about_me.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
+import './css/about_me.css';
+const AboutMe = () => {
+    return (
+        <div className='about-me-container'>
+            <div className='about-me-container-text'>
+                <div className='about-me-item'>
+                    <h1>About me</h1>
+                    <h1>Resume</h1>
+              
+                </div>
+                </div>
 
-const AboutMe = () => { 
-<div className='about-me-container-text'>
+        <div className='about-me-img-text-container'> 
 
-<div className='about-me-item'>
-    
-    <h1>About me</h1>
-    <h1>Resume</h1>
-     </div>
+            <img src="" alt="Khalil" className='about-me-img'  ></img>
+            <p> 
 
-    </div> 
+            Lorem ipsum dolor sit amet. Aut fuga sint 
+non alias voluptatem sed quibusdam conse
+quatur aut quam dolores et quisquam vitae a
+ possimus maxime rem velit ducimus! Sed
+
+
+Lorem ipsum dolor sit amet. Aut fuga sint 
+non alias voluptatem sed quibusdam conse
+quatur aut quam dolores et quisquam vitae a
+ possimus maxime rem velit ducimus! Sed
+
+
+            </p>
+            {/* Here comes a PDF file */}
+            <img alt="PDF File"></img>
+
+
+        </div>
+
+        </div>
+
+    );
+
 
 }
 
